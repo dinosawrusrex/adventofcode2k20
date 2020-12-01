b17b0fa0676c4850404937932c594831e3846d3f
dinosawrusrex <dennissaw12@gmail.com>
2020-12-01 00:42:31 -0800

    Refactor day 1: Use combinations, duh!

--- day1.py
+++ day1.py
@@ -1,30 +1,23 @@
-def multiply_pairwise_sum_to_2020(numbers, total=2020):
+import itertools
+import math
 
-    for number in numbers:
-        balance = total - number
-        if balance in numbers:
-            return number * balance
 
-def multiply_triples_to_2020(numbers):
+def multiply_combination_if_sum_to_2020(numbers, number_in_combination):
 
-    for i, number in enumerate(numbers):
-        balance = 2020 - number
-
-        multiple = multiply_pairwise_sum_to_2020(numbers[i+1:], total=balance)
-
-        if isinstance(multiple, int):
-            return multiple * number
+    for combo in itertools.combinations(numbers, number_in_combination):
+        if sum(combo) == 2020:
+            return math.prod(combo)
 
 
 if __name__ == '__main__':
 
-    test = [1721, 979, 366, 299, 675, 1456]
-    assert multiply_pairwise_sum_to_2020(test) == 514579
-    assert multiply_triples_to_2020(test) == 241861950
+    test = (1721, 979, 366, 299, 675, 1456)
+    assert multiply_combination_if_sum_to_2020(test, 2) == 514579
+    assert multiply_combination_if_sum_to_2020(test, 3) == 241861950
 
     with open('inputs/day1.txt') as f:
-        numbers = [int(n) for n in f.read().split('\n') if n]
+        numbers = tuple(int(n) for n in f.read().split('\n') if n)
 
-    print(multiply_pairwise_sum_to_2020(numbers))
-    print(multiply_triples_to_2020(numbers))
+    assert multiply_combination_if_sum_to_2020(numbers, 2) == 1006875
+    assert multiply_combination_if_sum_to_2020(numbers, 3) == 165026160
 
