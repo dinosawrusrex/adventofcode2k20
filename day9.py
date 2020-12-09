18fd80a55d28bb01078121c59f4cfcf5915181b3
dinosawrusrex <dennissaw12@gmail.com>
2020-12-09 00:04:17 -0800

    Refactor day 9
    Part 1
    - Use any for checking if two numbers within a preamble sums to the
    next.
    - Use recursion, adding 1 to head. We only need head since we can figure
    out the range of values with the preamble which is constant.
    
    Part 2
    - Slice the xmas list. No need to re-create list with range and
    indices..
    
    Remove commenting of solutions

--- day9.py
+++ day9.py
@@ -1,34 +1,24 @@
-def first_number_without_summable_pair(xmas, preamble=25):
-    head = preamble
-    while head < len(xmas):
-        start = head - preamble
-        for i in range(start, head):
-            balance = xmas[head] - xmas[i]
-            try:
-                xmas.index(balance, start, head)
-            except ValueError:
-                continue
-            else:
-                head += 1
-                break
+def first_number_without_summable_pair(xmas, head=None, preamble=25):
+    head = preamble if head is None else head
+    chunk = xmas[head - preamble: head]
 
-        # This else clause will only be executed
-        # if loop not terminated by break.
-        else:
-            return xmas[head]
+    if any(xmas[head]-i in chunk for i in chunk):
+        return first_number_without_summable_pair(xmas, head+1, preamble)
+
+    return xmas[head]
+
 
 def encryption_weakness(xmas, invalid):
-    start, end = 0, 1
+    start, end = 0, 2
 
-    while (total := sum(
-        contiguous_set := [xmas[x] for x in range(start, end)]
-    )) != invalid:
+    # Cannot recurse.. Keep hitting max recursion
+    while (total := sum((chunk := xmas[start:end]))) != invalid:
         if total < invalid:
             end += 1
         else:
             start += 1
 
-    return min(contiguous_set) + max(contiguous_set)
+    return min(chunk) + max(chunk)
 
 
 if __name__ == '__main__':
@@ -39,13 +29,11 @@ if __name__ == '__main__':
     ]
 
     assert first_number_without_summable_pair(test, preamble=5) == 127
-    # assert encryption_weakness(test, 127) == 62
+    assert encryption_weakness(test, 127) == 62
 
-    # with open('inputs/day9.txt') as f:
-    #     xmas = [int(l.strip()) for l in f.readlines() if l]
+    with open('inputs/day9.txt') as f:
+        xmas = [int(l.strip()) for l in f.readlines() if l]
 
-    # invalid = first_number_without_summable_pair(xmas)
-    # assert invalid == 542529149
-    # assert encryption_weakness(xmas, invalid) == 75678618
-
-
+    invalid = first_number_without_summable_pair(xmas)
+    assert invalid == 542529149
+    assert encryption_weakness(xmas, invalid) == 75678618
